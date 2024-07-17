5600d56f8f6de8ef044562434250355eff7f0434
Kevin Rognon <rognonk@gmail.com>
2024-07-17 22:04:12 +0200

    Ajout du formulaire de connection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import './global.css';
+import '../public/global.css';
 
 export default function RootLayout({
                                        children,
@@ -8,23 +8,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="fr">
-        <body>
-            <header className="bg-lime-500 text-lime-700 h-20 flex items-center justify-center">
-                <nav className="flex items-center justify-center gap-2">
-                    <h1 className="underline p-3 hover:bg-lime-200 rounded-full">
-                        <Link href="/">Russell</Link>
-                    </h1>
-                    <ul>
-                        <li className="underline p-3 hover:bg-lime-200 rounded-full">
-                            <Link href="/dashboard">Dashboard</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                {children}
-            </main>
-        </body>
+            <body>
+                <main>
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Link from "next/link";
+import LoginForm from "./components/login_form";
 
 export default function Page() {
     return (
         <>
-            <h1>Hello, Next.js!</h1>
+            <div className="flex flex-col items-center justify-center w-full">
+                <h1>Capitainerie De Russell</h1>
+                <p className="text-sm">Veuillez vous connecter pour continuer sur la gestion du site.</p>
+                <p className="text-sm">En cas de soucis, merci de contacter le support informatique.</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center w-full mt-4">
+
+                <h2>Se connecter</h2>
+                <LoginForm />
+
+            </div>
+
         </>
     )
 }

159cf7ff63e74db1bf6dcc491af05aac1e7869af
Kevin Rognon <rognonk@gmail.com>
2024-08-02 22:51:17 +0200

    WIP user page

--- app/dashboard/account/@form/page.tsx
+++ app/dashboard/account/@form/page.tsx
@@ -7,17 +7,22 @@ import { useUser } from "../../../../context/userContext";
 function Account() {
     const {user, setUser} = useUser();
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('Test');
+    }
+
     return (
         <>
-            <div className="grid place-items-center border-2 rounded-lg p-5 w-3/5 mt-5">
-                <form className="flex flex-col gap-2">
-                    <input className="border-2 rounded-3xl p-2" type="text" placeholder="Username"/>
+            <div className="grid place-items-center border-t-2 border-b-2 border-r-2 p-5 w-3/5 mt-5">
+                <form onSubmit={onSubmit} className="flex flex-col gap-2">
+                    <input className="border-2 rounded-3xl p-2 pl-4" type="text" placeholder="Username"/>
                     
-                    <input className="border-2 rounded-3xl p-2" type="email" placeholder="Email"/>
+                    <input className="border-2 rounded-3xl p-2 pl-4" type="email" placeholder="Email"/>
                     
-                    <input className="border-2 rounded-3xl p-2" type="password" placeholder="Password"/>
+                    <input className="border-2 rounded-3xl p-2 pl-4" type="password" placeholder="Password"/>
 
-                    <BlueButton onClick={console.log('Test')} text="Modifier"  />
+                    <BlueButton onClick={onSubmit} text="Modifier"  />
                 </form>
             </div>
         </>

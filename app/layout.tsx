1effd45edacda8e96c727e327664e7c3ca53c304
Kevin Rognon <rognonk@gmail.com>
2024-08-18 18:47:24 +0200

    WIP sur l'ajout d'une image de profil

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import '../public/global.css';
 import { UserProvider } from '../context/userContext';
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="fr">
             <body className='bg-cyan-950 text-cyan-100'>
--- components/ui/modal/ModalUploadFile.tsx
+++ components/ui/modal/ModalUploadFile.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useUser } from "../../../context/userContext";
 
 
-export default function ModalUploadFile({title, text, onClick}) {
+export default function ModalUploadFile({title, text, onClick, onSuccessUpload}) {
 
     const {user} = useUser();
     const [file, setFile] = useState(null);
@@ -13,7 +13,7 @@ export default function ModalUploadFile({title, text, onClick}) {
         e.preventDefault();
 
         const formData = new FormData();
-        formData.append('name', file);
+        formData.append('file', file);
         formData.append('userId', user.id);
 
         
@@ -23,7 +23,13 @@ export default function ModalUploadFile({title, text, onClick}) {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-            console.log(response.data);
+            if(response.status === 201) {
+                onSuccessUpload();
+                const update_avatar_url = await axios.patch(`${process.env.URL}/users/${user._id}/update/`, {
+                    avatar: `${process.env.URL}/${response.data.path}`
+                })
+                // window.location.reload();
+            }
         } catch (error) {
             console.error(error);
         }
@@ -39,8 +45,8 @@ export default function ModalUploadFile({title, text, onClick}) {
 
             <div onClick={onClick} className='absolute top-0 left-0 z-10 w-dvw h-dvh bg-black bg-opacity-70 flex justify-center items-center'>
                 <div onClick={stopPropagation} id="modalFile" className='flex items-center justify-center text-black bg-neutral-300 w-fit h-fit p-2 rounded-md'>
-                    <form onSubmit={handleSubmit} >
-                        <div className="flex flex-col ">
+                    <form onSubmit={handleSubmit} className="p-5" >
+                        <div className="flex flex-col gap-5">
                             <input onChange={(e) => {setFile(e.target.files[0])}} type="file" name="file" />
                             <BlueButton onClick={onsubmit} text="Uploader" />
                         </div>
